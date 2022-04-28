469e81e301651d8dfcdea654f26fb9c6fd47837d
YUJIN <yujinj704@gmail.com>
2022-04-28 16:46:03 +0900

    practice04 prob01 commmit

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,5 +2,62 @@ package prob01;
 
 public class Printer {
 
+	public int num;
+	public boolean b;
+	public double score;
+	public String name;
+	
+	
+	public Printer() {
+		this(0, false, 0.0, null);
+	}
+	
+	public Printer(int num,boolean b,double d,String name) {
+		this.num = num;
+		this.b = b;
+		this.score = d;
+		this.name = name;
+	}
+	
+	
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public boolean isB() {
+		return b;
+	}
+
+	public void setB(boolean b) {
+		this.b = b;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void println() {
+		System.out.println(num);
+		System.out.println(b);
+		System.out.println(score);
+		System.out.println(name);
+	}
+	
 
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -5,9 +5,11 @@ public class PrinterTest {
 	public static void main(String[] args) {
 		Printer printer = new Printer();
 
-		printer.println( 10 );
-		printer.println( true );
-		printer.println( 5.7 );
-		printer.println( "홍길동" );
+		printer.setNum( 10 );
+		printer.setB( true );
+		printer.setScore( 5.7 );
+		printer.setName( "홍길동" );
+		
+		printer.println();
 	}
 }
