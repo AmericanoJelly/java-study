76fac645cc28cd2b98383c70bfa8368b8e0ff2a8
YUJIN <yujinj704@gmail.com>
2022-04-28 13:29:23 +0900

    ArrayUtil commit

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -2,21 +2,30 @@ package chapter03;
 
 public class ArrayUtil {
 
+	//int 배열을 double 배열로 변환
 	public static double[] intToDouble(int[] a) {
 		double[] result = new double[a.length];
-		for(int i = 0; i < a.length; i++) {
+		for(int i=0; i < a.length; i++) {
 			result[i] = a[i];
 		}
-		
 		return result;
 	}
 
-	public static int[] doubleToint(double[] ds) {
-		return null;
+	//double 배열을 int 배열로 변환
+	public static int[] doubleToDouble(double[] ds) {
+		int[] result = new int[ds.length];
+		for(int i=0; i < ds.length; i++) {
+			result[i] = (int) Math.abs(ds[i]);
+		}
+		return result;
 	}
-
-	public static int[] concat(int[] is1, int[] is2) {
-		return null;
+	
+	//int 배열 두개를 연결한 새로운 배열 리턴
+	public static int[] concat(int[] is, int[] is2) {
+		int[] result = new int[is.length + is2.length];
+		System.arraycopy(is, 0, result, 0, is.length);
+		System.arraycopy(is2, 0, result, is.length, is2.length);
+		return result;
 	}
 
 }
--- chapter03/src/main/java/chapter03/ArrayUtilTest.java
+++ chapter03/src/main/java/chapter03/ArrayUtilTest.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 public class ArrayUtilTest {
 
 	public static void main(String[] args) {
-		double[] d1 = ArrayUtil.intToDouble(new int[]{10, 20, 30, 40});
-		// for(double d : d1) {
-		// 	System.out.print(d + " ");
-		// }
-		System.out.println(Arrays.toString(d1));
+		//int[] a1 = {10, 20, 30, 40};
+		//double[] d1 = ArrayUtil.intToDouble(a1);
 		
-		int[] a1 = ArrayUtil.doubleToint(new double[]{10.1, 20.2, 30.3, 40.4});
-		System.out.println(Arrays.toString(a1));
-
-		int[] a2 = ArrayUtil.concat(new int[]{1, 2, 3}, new int[]{4, 5, 6});
-		System.out.println(Arrays.toString(a2));
-
+		double[] d1 = ArrayUtil.intToDouble(new int[] {10, 20, 30, 40});
+		System.out.print(Arrays.toString(d1));
+		//for each문,  for( 각 요소 값 : 배열이나 컨테이너 값 )
+		/* for(double d : d1) {
+			System.out.print(d+" ");
+		} */
+	
+		int[] a1 = ArrayUtil.doubleToDouble(new double[] {10.1, 20.2, 30.3, 40.4});
+		System.out.print(Arrays.toString(a1));
+		
+		int[] a2 = ArrayUtil.concat(new int[] {1,2,3},new int[] {4,5,6});
+		System.out.print(Arrays.toString(a2));
 	}
-
 }
