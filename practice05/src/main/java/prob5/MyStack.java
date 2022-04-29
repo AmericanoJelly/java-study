d8ddd3c45459909a70a0242633e1af7b763c8ff8
YUJIN <yujinj704@gmail.com>
2022-04-29 17:14:37 +0900

    practice05 ):

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,22 @@
 package prob5;
 
 public class MyStack {
+	private int top = 0;//관리
+	private String[] buffer;
+	
+	public MyStack(int size) {
+		buffer = new String[size];
+	}
+	
+	public void push(String item) {
+		
+	}
+	
+	public void pop() {
+		
+	}
+	
+	public boolean isEmpty(){
+		
+	}
 }
