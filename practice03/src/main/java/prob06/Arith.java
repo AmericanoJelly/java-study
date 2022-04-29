7464f718ce6fea31eb67e3ac43a87a1068486944
YUJIN <yujinj704@gmail.com>
2022-04-29 17:14:03 +0900

    살려주세요오

---
+++ practice03/src/main/java/prob06/Arith.java
@@ -0,0 +1,15 @@
+package prob06;
+
+public class Arith {
+	
+	private int a;
+	private int b;
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	
+	public abstract int calculate();
+
+}
