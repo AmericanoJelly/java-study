74108a9f165c1741066384c2cf17a3b439300ae0
yujin <jyj6010@naver.com>
2022-05-02 00:16:10 +0900

    5번 푸는중...

--- pracitce05/src/main/java/prob5/MyStack.java
+++ pracitce05/src/main/java/prob5/MyStack.java
@@ -7,21 +7,24 @@ public class MyStack {
 	public MyStack(int size) {
 		buffer = new String[size];
 	}
-	
+
 	public void push(String item) {
-		
+		buffer[++top] = item;
+	}
+
+	public String pop() throws MyStackException{
+		if(top == 0) {
+			throw new MyStackException();
+			}
+		String item = buffer[top];
+		buffer[top] = null;
+		top--;
+		return item;
 	}
 	
-	public String pop() {
-		return null;
+	public boolean isEmpty() {		
+		return (top == 0);
 	}
 	
-	public boolean isEmpty() {
-		return false;
-		
-	}
 	
-	if() {
-		throw new MyStackException();
-	}
 }
--- pracitce05/src/main/java/prob5/MyStackException.java
+++ pracitce05/src/main/java/prob5/MyStackException.java
@@ -5,15 +5,12 @@ public class MyStackException extends Exception {
 	private static final long serialVersionUID = 1L;
 	
 	public MyStackException() {
-		super("Prob5.MyStackException: stack is empty");
+		super("stack is empty");
 	}
 	
 	
 	public MyStackException(String message) {
 		super(message);
 	}
-	
-	
-
 
 }
