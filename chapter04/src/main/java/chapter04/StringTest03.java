0cb9a368071a2e581be63f6d84ba10aaec2fded5
YUJIN <yujinj704@gmail.com>
2022-05-02 09:47:07 +0900

    commit

--- chapter04/src/main/java/chapter04/StringTest03.java
+++ chapter04/src/main/java/chapter04/StringTest03.java
@@ -3,8 +3,12 @@ package chapter04;
 public class StringTest03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// String method들..
+		String s1 = "aBcABCabcAbc";
+		System.out.println(s1.length());
+		System.out.println(s1.charAt(2));
+		System.out.println(s1.indexOf("abc"));
+		
 	}
 
 }
