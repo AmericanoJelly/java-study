6ebf4da4717b4b24532a96ad4abc6ba6c16a3575
YUJIN <yujinj704@gmail.com>
2022-05-03 17:37:28 +0900

    final #1

--- chapter04/src/main/java/io/PhoneList02.java
+++ chapter04/src/main/java/io/PhoneList02.java
@@ -2,16 +2,14 @@ package io;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class PhoneList02 {
-
 	public static void main(String[] args) {
 		Scanner scanner = null;
+		
 		try {
 			File file = new File("phone.txt");
 			
@@ -19,13 +17,12 @@ public class PhoneList02 {
 				System.out.println("file not found");
 				return;
 			}
-			
+	
 			System.out.println("==========파일정보===========");
 			System.out.println(file.getAbsolutePath());
 			System.out.println(file.length() + "bytes");
-			//SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(file.lastModified()))); //타임스탬프
-			
+			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(file.lastModified())));
+	
 			System.out.println("==========전화번호===========");
 			scanner = new Scanner(file);
 			
@@ -35,13 +32,12 @@ public class PhoneList02 {
 				String phone2 = scanner.next();
 				String phone3 = scanner.next();
 				
-				System.out.println(name + ":" + phone1 + "-" + phone2 + "-"+ phone3);
+				System.out.println(name + ":" + phone1 + "-" + phone2 + "-" + phone3);
 			}
-		}catch(FileNotFoundException ex){
-			System.out.println("File Not Found: " + ex);
-		}finally {
+		} catch(FileNotFoundException ex) {
+			System.out.println("File Not Found:" + ex);
+		} finally {
 			scanner.close();
 		}
 	}
 }
-
