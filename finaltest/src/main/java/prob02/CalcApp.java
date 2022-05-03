ba61f359902e6a7036ac4cb9ad8b80c28b7d6e30
Yujin Jeong <87961179+AmericanoJelly@users.noreply.github.com>
2022-05-03 23:41:07 +0900

    정수 계산기

--- finaltest/src/main/java/prob02/CalcApp.java
+++ finaltest/src/main/java/prob02/CalcApp.java
@@ -28,7 +28,24 @@ public class CalcApp {
 			Arithmetic arithmetic = null;
 			
 			/* 코드 작성 */
-			
+			switch( tokens[ 2 ] ) {
+			case "+" : {
+				arithmetic = new Add();
+				break;
+			}
+			case "-" : {
+				arithmetic = new Sub();
+				break;
+			}
+			case "*" : {
+				arithmetic = new Mul();
+				break;
+			}
+			case "/" : {
+				arithmetic = new Div();
+				break;
+			}
+			}
 			int result = arithmetic.calculate(lValue, rValue);
 			System.out.println( ">> " + result );
 		}
