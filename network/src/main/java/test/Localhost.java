499806efeec9d204e83ca332dc55d2444f9c178f
YUJIN <yujinj704@gmail.com>
2022-05-04 16:20:12 +0900

    소켓프로그래밍#1

---
+++ network/src/main/java/test/Localhost.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Localhost {
+
+	//소켓프로그래밍
+	public static void main(String[] args) {
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			
+			String hostName = inetAddress.getHostName();
+			String hostAddress = inetAddress.getHostAddress();
+			
+			System.out.println(hostName); //컴퓨터이름
+			System.out.println(hostAddress); //첫번째 네트워크주소
+		
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
