3838fc5e250bc955c05fb4fc03bfb91242a38fa8
yujin <jyj6010@naver.com>
2022-05-08 01:56:30 +0900

    NSLookup #finish

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,9 +1,35 @@
 package util;
 
-public class NSLookup {
-	public static void main(String[] args) {
-		
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class NSLookup {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+				System.out.print(">");
+				String line = scanner.nextLine();
+				
+				if("exit".equals(line)) {
+					break;
+				}
+				
+				InetAddress[] address = InetAddress.getAllByName(line);
+				
+				for(int i=0; i<address.length; i++) {
+					
+				System.out.println(line+": "+address[i].getHostAddress());
+				
+				}
+				
+			} catch(UnknownHostException ex) {
+				ex.printStackTrace();
+			}	
+		}
+		scanner.close();
 	}
 
 }
