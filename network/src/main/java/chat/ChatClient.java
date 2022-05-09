5d394b3621111bbcb7cbb2ddb999058527ca95e0
YUJIN <yujinj704@gmail.com>
2022-05-09 17:57:09 +0900

    에러투성이커밋하기

--- network/src/main/java/chat/ChatClient.java
+++ network/src/main/java/chat/ChatClient.java
@@ -1,9 +1,44 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatClient {
 
 	public static void main(String[] args) {
-		// 닉네임 입력받기
-
-	}
+		Socket socket = null;
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(System.in);
+			socket = new Socket();
+			
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8"), true);
+		
+			while(true) {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+				
+				if("quit".equals(input) == true) {
+					break;
+				}else {
+					//메시지처리
+				}
+				
+		} catch (IOException e) {
+			System.out.println("[client] error: "+e);
+		}finally {
+			//자원정리
+		}
+				public static void log(String log) {
+					System.out.println("[Echo Client]" + log);
+				}
 }
