7566cba72b91e026fc2e274a43d7518a28c76132
yujin <jyj6010@naver.com>
2022-05-11 01:06:42 +0900

    ChatThread ):

--- network/src/main/java/chat/ChatServerThread.java
+++ network/src/main/java/chat/ChatServerThread.java
@@ -23,6 +23,7 @@ public class ChatServerThread extends Thread {
 
 	@Override
 	public void run() {
+		//1. Remote Host Information
 		InetSocketAddress inetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
 		String remoteHostAddress = inetSocketAddress.getAddress().getHostAddress();
 		int remoteHostPort = inetSocketAddress.getPort();
@@ -30,34 +31,36 @@ public class ChatServerThread extends Thread {
 		ChatServer.log("connected by client [ "+ remoteHostAddress + ": "+remoteHostPort +" ]");
 		
 		try {
-		//스트림얻기
+		//2. 스트림 얻기
 		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 		PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8"), true);
 		
-		//요청처리
-		while(true) {
-			String request = br.readLine();
-			
-			if(request == null) {
-				ChatServer.log("클라이언트로 부터 연결 끊김");
-				doQuit(pw);
-				break;
+			//3. 요청 처리 
+			while(true) {
+				String request = br.readLine();
+				
+				if(request == null) {
+					ChatServer.log("클라이언트로 부터 연결 끊김");
+					doQuit(pw);
+					break;
+				}
+				
+				// 4. 프로토콜 분석
+				String[] tokens = request.split(":");
+				if("join".equals(tokens[0])) {
+					doJoin(tokens[1],pw);
+					
+				} else if("message".equals(tokens[0])) {
+					doMessage(tokens[1]);
+					
+				} else if("quit".equals(tokens[0])) {
+					doQuit(pw);
+
+				} 
 			}
-			
-			String[] tokens = request.split(":");
-			if("join".equals(tokens[0])) {
-				doJoin(tokens[1],pw);
-				
-			} else if("message".equals(tokens[0])) {
-				doMessage(tokens[1]);
-				
-			} else if("quit".equals(tokens[0])) {
-				doQuit(pw);
-			} 
-		}
-		}catch(IOException ex) {
-				System.out.println(this.nickname + "님이 채팅방을 나갔습니다.");
-		}
+			}catch(IOException ex) {
+					System.out.println(this.nickname + "님이 채팅방을 나갔습니다.");
+			}
 	}
 
 	private void doJoin(String nickName, Writer writer) {
@@ -88,7 +91,7 @@ public class ChatServerThread extends Thread {
 	   }
 
 	private void doQuit(Writer writer) {
-		removeWriter( writer );
+		removeWriter(writer);
 		
 		String data = this.nickname + "님이 퇴장 하였습니다.";
 		broadcast(data);	
--- network/src/main/java/chat/chatClientThread.java
+++ network/src/main/java/chat/chatClientThread.java
@@ -1,14 +1,30 @@
 package chat;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 
 public class chatClientThread extends Thread {
-	private BufferedReader bufferReader;
+	private Socket socket;
 	
+	public chatClientThread(Socket socket) {
+		this.socket = socket;
+	}
+
+
 	@Override
 	public void run() {
-		
+		/* reader를 통해 읽은 데이터 콘솔에 출력하기 (message 처리) */
+		try {
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			while(true) {
+				System.out.println(br.readLine());
+			}
+		} catch(IOException e) {
+			System.out.println("error: " + e);
+		}
 	}
-	
-	
 }
